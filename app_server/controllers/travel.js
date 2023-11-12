46bbe2ef8beee23c6ab91021f37b7719903d28d5
VictorWRX <victor.gallego@yahoo.com>
2023-11-12 13:09:21 -0500

    Static HTML to Templates with JSON

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,7 +1,12 @@
+
+const fs = require('fs');
+const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
+
 /* GET travel view */
 const travel = (req, res) => {
     pageTitle = ' TRAVLR - Travlr';
-    res.render('travel', { title: pageTitle});
+    res.render('travel', {title: pageTitle, trips});
    };
    
    module.exports = {
